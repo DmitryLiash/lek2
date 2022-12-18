381dcbd4bf0fd1a450c12b3e77fa4b40da153749
DmitryL <dimak.pax@gmail.com>
2022-12-18 20:15:38 +0600

    001v3

---
+++ Example001v3/Program.cs
@@ -0,0 +1,36 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {15, 42, 39, 12, 93, 323, 2, 23, 33 };
+
+int otvet = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(otvet);
+
+
+
+// int a1 = 15;
+// int b1 = 42; 
+// int c1 = 39;
+// int a2 = 12;
+// int b2 = 93;
+// int c2 = 323;
+// int a3 = 2;
+// int b3 = 23;
+// int c3 = 33;
+
+// int max1 = Max(a1,b1,c1);
+// int max2 = Max(a2,b2,c2);
+// int max3 = Max(a3,b3,c3);
+// int max = Max(max1, max2, max3);
+
+// int max = Max(Max(a1, b1, c1),Max(a2, b2, c2),Max(a3, b3, c3));
+
+// Console.WriteLine(max);
