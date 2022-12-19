108d274c0d081ad32dd8c2134d709b34a041b595
DmitryL <dimak.pax@gmail.com>
2022-12-19 13:42:37 +0600

    s001

---
+++ sem2001/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(10,100); // ot 10 do 99. 100 ne vkl
+
+Console.WriteLine(number);
+
+if (number / 10 > number % 10) Console.WriteLine(number / 10);
+else Console.WriteLine(number % 10);
+
+// Console.WriteLine(number / 10 > number % 10 ?
+// number / 10 :
+// number % 10);
